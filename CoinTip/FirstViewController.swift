3fcb9cecc56941d7a86a0fdc74ac527abb01040a
Eugene Zaikonnikov <eugene@funcall.org>
2018-06-05 21:14:33 +0200

    Initial Commit

---
+++ CoinTip/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  CoinTip
+//
+//  Created by Eugene Zaikonnikov on 05/06/2018.
+//  Copyright © 2018 Eugene Zaikonnikov. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CoinTipTests/CoinTipTests.swift
@@ -0,0 +1,36 @@
+//
+//  CoinTipTests.swift
+//  CoinTipTests
+//
+//  Created by Eugene Zaikonnikov on 05/06/2018.
+//  Copyright © 2018 Eugene Zaikonnikov. All rights reserved.
+//
+
+import XCTest
+@testable import CoinTip
+
+class CoinTipTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
