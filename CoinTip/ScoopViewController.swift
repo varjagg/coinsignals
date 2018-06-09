63497a4033f9992737d5e577f369f96579569ef7
Eugene Zaikonnikov <eugene@funcall.org>
2018-06-09 18:05:06 +0200

    pull to update

--- CoinTip/ScoopViewController.swift
+++ CoinTip/ScoopViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import os.log
 
 struct ScoopData: Codable {
-    let price: String?
+    let price: Float?
     let trend: String?
     let signal: String?
 }
@@ -18,18 +18,37 @@ struct ScoopData: Codable {
 class ScoopViewController: UIViewController {
     
     //MARK: properties
-    var scoopArray = [String] ()
-
+    @IBOutlet var scrollView: UIScrollView!
     @IBOutlet weak var rate: UILabel!
     @IBOutlet weak var trend: UILabel!
     @IBOutlet weak var advice: UILabel!
     
+    var scoopArray = [String] ()
+    
+    lazy var refresh: UIRefreshControl = {
+        let refreshControl = UIRefreshControl()
+        refreshControl.tintColor = .white
+        refreshControl.addTarget(self, action: #selector(self.didPullToRefresh), for: .valueChanged)
+        return refreshControl
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        loadScoop()
+        scrollView.refreshControl = refresh
     }
 
+    @objc func didPullToRefresh() {
+        loadScoop()
+        refresh.endRefreshing()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        loadScoop()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -39,7 +58,9 @@ class ScoopViewController: UIViewController {
     private func loadScoop() {
         guard let url = URL(string: "https://farca-pioneer.funcall.org/scoop.json") else { return }
         
-        URLSession.shared.dataTask(with: url) { (data, response
+        let request = URLRequest(url: url, cachePolicy: URLRequest.CachePolicy.reloadIgnoringLocalCacheData, timeoutInterval: 60.0)
+        
+        URLSession.shared.dataTask(with: request) { (data, response
             , error) in
             guard let data = data else { return }
             do {
@@ -49,7 +70,7 @@ class ScoopViewController: UIViewController {
                 DispatchQueue.main.sync {
                     
                     if let price = scoopData.price {
-                        self.rate.text = price
+                        self.rate.text = "Rate: \(price)"
                     }
                     
                     if let trend = scoopData.trend {
@@ -58,15 +79,24 @@ class ScoopViewController: UIViewController {
                     
                     if let signal = scoopData.signal {
                         self.advice.text = signal
+                        switch signal {
+                        case "SELL":
+                            self.view.backgroundColor = .red
+                        case "BUY":
+                            self.view.backgroundColor = .red
+                        case "NEARING":
+                            self.view.backgroundColor = .yellow
+                        default:
+                            self.view.backgroundColor = .blue
+                        }
                     }
-                    
-
                 }
-                
-            } catch let _ {
-                os_log("Could not proceed",  log: OSLog.default, type: .error)
+            } catch _ {
+                let alert = UIAlertController(title: "Network Error", message: "Could not connect to trading service", preferredStyle: UIAlertControllerStyle.alert)
+                alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: {(alert: UIAlertAction!) in print("Network failure")}))
+                self.present(alert, animated: true, completion: nil)
+                os_log("Could not parse scoop",  log: OSLog.default, type: .error)
             }
         }.resume()
-        
     }
 }
