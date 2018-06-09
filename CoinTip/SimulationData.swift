a416070500634c8855a3aa1234fe23389228b0d6
Eugene Zaikonnikov <eugene@funcall.org>
2018-06-09 18:07:42 +0200

    simulation data settings

---
+++ CoinTip/SimulationData.swift
@@ -0,0 +1,18 @@
+//
+//  SimulationData.swift
+//  CoinTip
+//
+//  Created by Eugene Zaikonnikov on 07/06/2018.
+//  Copyright © 2018 Eugene Zaikonnikov. All rights reserved.
+//
+
+import Foundation
+
+class SimulationData {
+    //MARK: Properties
+    
+    var dateFrom: Date = Date()
+    var dateTo: Date = Date()
+    var amount: Float = 0.01
+    
+}
