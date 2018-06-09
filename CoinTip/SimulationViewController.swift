5b46b8300215ff56d6ccc8d69bf256a0864725ce
Eugene Zaikonnikov <eugene@funcall.org>
2018-06-09 18:05:44 +0200

    pass ledger, handle keyboard

--- CoinTip/SimulationViewController.swift
+++ CoinTip/SimulationViewController.swift
@@ -9,43 +9,90 @@
 import UIKit
 import os.log
 
-struct LedgerData: Codable {
-    let dts: Int32?
-    let type: String?
-    let price: Float?
+struct SignalsResponse: Decodable {
+    let timestamp: Int
+    let entries: [SignalsData]
 }
 
-class SimulationViewController: UIViewController {
+struct SignalsData: Decodable {
+    let dts: Int
+    let type: String
+    let price: Float
+}
+
+class SimulationViewController: UIViewController, UITextFieldDelegate {
     //MARK: properties
-    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var periodStart: UIDatePicker!
+    @IBOutlet weak var periodEnd: UIDatePicker!
+    @IBOutlet weak var startAmount: UITextField!
+    
+    var preparedLedger: Ledger?
     
     //MARK: actions
     @IBAction func runSimulation(_ sender: UIButton) {
+        // Create the Activity Indicator
+        let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+        
+        view.addSubview(activityIndicator)
+        activityIndicator.frame = view.bounds
         activityIndicator.startAnimating()
-        loadLedger()
+        loadSignals()
         activityIndicator.stopAnimating()
+        activityIndicator.removeFromSuperview()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+    startAmount.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        guard let tradeController = segue.destination as? SimulationTableViewController else {
+            fatalError("Unexpected destination: \(segue.destination)")
+        }
+        
+        guard let sendButton = sender as? UIButton else {
+            fatalError("Unexpected sender: \(String(describing: sender))")
+        }
+        
+        guard preparedLedger != nil else {
+            fatalError("Missing ledger data")
+        }
+        tradeController.ledger = preparedLedger!
+        
+    }
+    
+    //MARK: UITextField
 
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+
+        textField.resignFirstResponder()
+        return true
+    }
+    
     //MARK: private
     
-    private func loadLedger() {
+    private var simulationData: SimulationData = SimulationData()
+    
+    private func saveSimulationData() {
+        simulationData.amount = 0
+    }
+    
+    private func loadSignals() {
         guard let url = URL(string: "https://farca-pioneer.funcall.org/ledger.json") else { return }
         
         URLSession.shared.dataTask(with: url) { (data, response, error) in
             guard let data = data else { return }
             do {
                 let decoder = JSONDecoder()
-                let ledgerData = try decoder.decode(LedgerData.self, from: data)
+                let ledgerResponse = try decoder.decode(SignalsResponse.self, from: data)
                 
                 DispatchQueue.main.sync {
       /*
@@ -53,10 +100,13 @@ class SimulationViewController: UIViewController {
                         self.rate.text = price
                     }
         */
-                    os_log("w00t",  log: OSLog.default, type: .debug)
                 }
-            } catch let _ {
-                os_log("Could not proceed",  log: OSLog.default, type: .error)
+            } catch _ {
+                let alert = UIAlertController(title: "Network Error", message: "Could not connect to trading service", preferredStyle: UIAlertControllerStyle.alert)
+                alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: {(alert: UIAlertAction!) in print("Network failure")}))
+                self.present(alert, animated: true, completion: nil)
+                
+                os_log("Could not parse ledger",  log: OSLog.default, type: .error)
             }
             }.resume()
         
